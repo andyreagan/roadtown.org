982fe82e15af221e869fea94b91c39d246d1e006
Andy Reagan <andy@andyreagan.com>
2023-08-06 22:05:59 -0400

    Add a sticky header with the links.

--- turkeytrot/urls.py
+++ turkeytrot/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import hello
+from .views import *
 
 urlpatterns = [
     path('', hello, name='hello'),
+    path('2', hello2, name='hello2'),
 ]
 
--- turkeytrot/views.py
+++ turkeytrot/views.py
@@ -9,5 +9,11 @@ from django.template.response import TemplateResponse
 def hello(request):
     # return HttpResponse('<center><h2>Roadtown Turkey Trot</h2></center>')
     return TemplateResponse(
+        request, "trot2023bootstrap.html", {}
+    )    
+
+
+def hello2(request):
+    return TemplateResponse(
         request, "trot2023.html", {}
     )    
